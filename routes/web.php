1ea5eacdb245b4cc3314ee476f999265adef0a31
Throab <93912311+Throab@users.noreply.github.com>
2024-03-15 17:02:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +21,13 @@ Route::get('/register',[AuthController::class,'showFormRegister'])->name('showFo
 Route::post('/register',[AuthController::class,'register'])->name('register');
 Route::get('/login',[AuthController::class,'showFormLogin'])->name('showFormLogin');
 Route::post('/login',[AuthController::class,'login'])->name('login');
-Route::get('logout',[AuthController::class,'logout'])->name('logout');
-Route::get('/search', 'HomeController@search')->name('search');
-Route::get('forgot-password',[ForgotPasswordController::class,'ShowForgotPassPage'])->name('showForgotPassPage');
-
+Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+Route::get('/forgot-password',[ForgotPasswordController::class,'showForgotPassPage'])->name('showForgotPassPage');
+Route::post('/forgot-password',[ForgotPasswordController::class,'checkExistedEmail'])->name('checkExistedEmail');
+Route::get('/forgot-password/enter-otp',[ForgotPasswordController::class,'showEnterOTPPage'])->name('showEnterOTPPage');
+Route::post('/forgot-password/sending-otp',[ForgotPasswordController::class,'sendOtp'])->name('sendOtp');
+Route::post('/forgot-password/enter-otp',[ForgotPasswordController::class,'verifiedOtp'])->name('verifiedOtp');
+Route::get('/forgot-password/new-password',[ResetPasswordController::class,'showNewPassPage'])->name('showNewPassPage');
+Route::post('/forgot-password/new-password',[ResetPasswordController::class,'setNewPass'])->name('setNewPass');
+Route::get('user/activation/{token}', [AuthController::class,'activateUser'])->name('user.activate');
+Route::get('/verify-success',[VerificationController::class,'showSuccess'])->name('verify-success');
